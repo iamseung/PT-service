b98be791a19ec938afb1dec41de974e1207a4946
iamseung <seesungseok@naver.com>
2024-02-01 22:03:27 +0900

    # 유저 엔티티 추가

--- src/main/java/com/dev/pass/repository/user/UserEntity.java
+++ src/main/java/com/dev/pass/repository/user/UserEntity.java
@@ -1,17 +1,28 @@
 package com.dev.pass.repository.user;
 
+import com.dev.pass.repository.BaseEntity;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
+import java.util.Map;
 
 @Getter
 @Setter
 @ToString
 @Entity
 @Table(name = "user")
-public class UserEntity {
+/**
+ * json의 타입을 정의!
+ * hibernate-types, 의존성 추가
+ * implementation 'com.vladmihalcea:hibernate-types-52:2.19.2'
+ */
+@TypeDef(name = "json", typeClass = JsonType.class)
+public class UserEntity extends BaseEntity {
     @Id
     private String userId;
 
@@ -19,4 +30,18 @@ public class UserEntity {
     @Enumerated(EnumType.STRING)
     private UserStatus status;
     private String phone;
+
+    // json 형태로 저장되어 있는 문자열 데이터를 Map으로 매핑
+    @Type(type = "json")
+    private Map<String, Object> meta;
+
+    public String getUuid() {
+        String uuid = null;
+        if (meta.containsKey("uuid")) {
+            uuid = String.valueOf(meta.get("uuid"));
+        }
+        return uuid;
+
+    }
+
 }
