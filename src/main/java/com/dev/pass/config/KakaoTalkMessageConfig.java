3f2964a286ae3bd9f115e4c2e88ec9085ab847c9
iamseung <seesungseok@naver.com>
2024-02-01 22:05:56 +0900

    # 카카오 알림톡 기능 구현 - 카카오 알림톡 관련 Config, Request, Response 구현 - Batch 로직 구현

---
+++ src/main/java/com/dev/pass/config/KakaoTalkMessageConfig.java
@@ -0,0 +1,17 @@
+package com.dev.pass.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@Getter
+@Setter
+@Component
+// application.yml 의 kakaotalk 값 조회
+@ConfigurationProperties(prefix = "kakaotalk")
+public class KakaoTalkMessageConfig {
+    private String host;
+    private String token;
+}
