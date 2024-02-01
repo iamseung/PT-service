57341f70bb80e4b18b3163e998281e6d2c4ad704
iamseung <seesungseok@naver.com>
2024-02-01 21:59:33 +0900

    # 예약 엔티티 추가 - 예약 엔티티 추가로 인한 Repository, Status 같이 업로드 진행

---
+++ src/main/java/com/dev/pass/repository/booking/BookingRepository.java
@@ -0,0 +1,20 @@
+package com.dev.pass.repository.booking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+
+public interface BookingRepository extends JpaRepository<Integer, BookingEntity> {
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE BookingEntity b" +
+            "          SET b.usedPass = :usedPass," +
+            "              b.modifiedAt = CURRENT_TIMESTAMP" +
+            "        WHERE b.passSeq = :passSeq")
+    int updateUsedPass(
+            @Param("passSeq") Integer passSeq,
+            @Param("usedPass") boolean usedPass);
+}
